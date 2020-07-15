a9f8a010b8f1ccfbb1a69cf5ddd3de14e7e8b51d
lenovo <fern>
2020-07-15 15:02:14 +0800

    啥

---
+++ app/src/main/java/com/example/myappersent/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.myappersent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
